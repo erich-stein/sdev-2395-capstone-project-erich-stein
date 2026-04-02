539124adb79dcb99de3073a5b0987ebeb67ab97f
Erich Stein <erichstein@sheridan.edu>
2026-04-02 21:40:06 +0000

    Installed flask-cors and flask-restful

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from config import Config
+from flask_cors import CORS
 
 app = Flask(__name__)
+app.config.from_object(Config)
+CORS(app)
 
 @app.route("/")
 def hello_world():
